f580c0504ae394f39ab8ad4461c8f2112ba3843c
prenaissance <alexandru.andries@isa.utm.md>
2023-09-23 17:04:55 +0300

    ui(landing): make layout for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
-import { mohave, mplus1p, poppins } from "~/utilities/fonts";
-import { Button, Divider, Flex, HStack, Text, chakra } from "@chakra-ui/react";
+import { mohave, poppins } from "~/utilities/fonts";
+import {
+  Button,
+  Divider,
+  Flex,
+  HStack,
+  Heading,
+  Text,
+  chakra,
+} from "@chakra-ui/react";
 import { Link } from "@chakra-ui/next-js";
 
 export default function Home() {
@@ -16,11 +24,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Flex w="100wv" h="100wh" flexDirection="column">
+      <Flex w="auto" minH="100dvh" flexDirection="column" overflowX="hidden">
         <HStack
           as="nav"
           bgColor="primary.main"
-          w="100%"
+          w="auto"
           h="5rem"
           roundedBottom="3xl"
           px="3.5rem"
@@ -68,7 +76,91 @@ export default function Home() {
             </Button>
           </HStack>
         </HStack>
-        <chakra.main className={poppins.className}>Hi</chakra.main>
+        <chakra.main
+          p="1rem"
+          flexGrow={1}
+          maxW="container.lg"
+          className={poppins.className}
+          display="grid"
+          gridTemplateColumns={{ base: "1fr", lg: "1fr 1.5fr" }}
+          alignSelf="center"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <chakra.section
+            display="grid"
+            gridTemplateColumns={{
+              base: "1fr 1fr",
+              lg: "1fr",
+            }}
+            gap="1rem"
+            mr={{
+              base: "0",
+              lg: "-1rem",
+            }}
+          >
+            <Heading
+              color="black"
+              fontSize="6xl"
+              textShadow={{
+                base: "0 0 5px rgba(0, 0, 0, 0.2)",
+                lg: "0 0 10px rgba(0, 0, 0, 0.2)",
+              }}
+              fontWeight={500}
+              lineHeight="0.9"
+            >
+              Warehouse Management System
+            </Heading>
+            <Text
+              alignSelf={{ base: "flex-end", lg: "flex-start" }}
+              color="primary.text"
+              fontSize="sm"
+              noOfLines={10}
+              mr={{
+                base: "0",
+                lg: "2rem",
+              }}
+            >
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using
+            </Text>
+            <Button
+              rounded="full"
+              bgColor="secondary.main"
+              _hover={{
+                bgColor: "secondary.hover",
+                textDecoration: "none",
+              }}
+              _focus={{
+                bgColor: "secondary.focus",
+              }}
+              w="fit-content"
+              fontWeight={400}
+              p="1.5rem"
+            >
+              Get started &#10230;
+            </Button>
+          </chakra.section>
+          <chakra.div
+            justifySelf="center"
+            p={{
+              base: "2rem",
+              lg: "0",
+            }}
+          >
+            <Image
+              style={{
+                objectFit: "contain",
+              }}
+              width={767}
+              height={494}
+              alt="Warehouse"
+              src="/images/landing/warehouse.png"
+            />
+          </chakra.div>
+        </chakra.main>
       </Flex>
     </>
   );
