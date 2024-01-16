68658b8b63d76e881cb1730d3ac035a02772568f
prenaissance <alexandru.andries@isa.utm.md>
2024-01-16 22:59:47 +0200

    chore(hasura): add seed data for categories and products

---
+++ hasura/seeds/pg/1705436506991_seed_categories.sql
@@ -0,0 +1,6 @@
+SET check_function_bodies = false;
+INSERT INTO public.categories (id, name, created_at) VALUES 
+    ('ec71a486-2c78-421e-9a97-5c055b5f4cbc', 'Smartphones', '2024-01-16 20:15:20.558021+00'),
+    ('2f58caf2-b02e-4766-b9bb-50f944ff62bf', 'Hygiene', '2024-01-16 20:15:45.691394+00'),
+    ('629e3d90-76de-46a1-8a73-638ef15ded73', 'Clothing', '2024-01-16 20:16:00.621899+00'),
+    ('e2917718-bc19-41ba-aed1-cba38c782280', 'Furniture', '2024-01-16 20:16:50.603719+00');
